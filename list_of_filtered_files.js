f14e44d0b21216a273b5875c7e8af3bb6ef79d62
Cleber Dantas <cleberwd@gmail.com>
2014-07-09 16:38:19 -0300

    adding the 'FILTERED LS' exercise

---
+++ list_of_filtered_files.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var path = require('path');
+
+fs.readdir(process.argv[2], function(err, list){
+	for (var i = 0; i < list.length; i++) {
+		if(path.extname(list[i]) === '.' + process.argv[3]){
+			console.log(list[i]);
+		}
+	};
+});
